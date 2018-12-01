638b84539c93d925fb7f1ab80c92725a959d7fb5
Jacob Benson <Jacob.Benson@siriuscom.com>
2018-12-01 17:37:39 -0600

    change path value

--- images/psdeploy.ps1
+++ images/psdeploy.ps1
@@ -1,6 +1,6 @@
 $rg="jacobbenson-rg"
 $job = 'job.' + ((Get-Date).ToUniversalTime()).tostring("MMddyy.HHmm")
-$template="C:\users\jacobbe\gir\arm-workshop\jblab1\azuredeploy.json"
-$parms="C:\users\jacobbe\gir\arm-workshop\jblab1\azuredeploy.parameters.json"
-$storageAccount = (New-AzureRmResourceGroupDeployment -Name $job -TemplateParameterFile $parms -TemplateFile $template -ResourceGroupName $rg).Outputs.storageAccount.Value
+$template="C:\users\jacobbe\git\arm-workshop\jblab1\azuredeploy.json"
+$parms="C:\users\jacobbe\git\arm-workshop\jblab1\azuredeploy.parameters.json"
+$storageAccount = (New-AzResourceGroupDeployment -Name $job -TemplateParameterFile $parms -TemplateFile $template -ResourceGroupName $rg).Outputs.storageAccount.Value
 Write-Output "Storage account $storageAccount has been created."
