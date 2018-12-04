e6f10b45dc26fab07eba8ca8f89fbe7242639baf
Jacob Benson <vhusker@users.noreply.github.com>
2018-12-04 13:17:15 -0600

    forgot to save again

---
+++ deploy.ps1
@@ -0,0 +1,13 @@
+Login-AzAccount
+Get-AzSubscription
+#This selects the free trial subscription
+Select-AzSubscription -Subscription 'f30702c5-148a-4ea6-baec-cca1d09e6c14'
+
+$rg = "lab2"
+$template = "C:\users\jacobbe\git\arm-workshop\jblab2\azuredeploy.json"
+$parms = "C:\users\jacobbe\git\arm-workshop\jblab2\azuredeploy.parameters.json"
+$job = 'job.' + ((Get-Date).ToUniversalTime()).tostring("MMddyy.HHmm")
+
+Test-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $parms -Mode Incremental
+
+New-AzResourceGroupDeployment -Name $job -ResourceGroupName $rg -TemplateFile $template -TemplateParameterFile $parms -Mode Incremental -DeploymentDebugLogLevel All -Verbose
